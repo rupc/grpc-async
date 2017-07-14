9c92894925fb9910278fead93fad586f508f9164
yongrae <neurosilver@gmail.com>
2017-07-14 14:35:19 +0900

    Client sends multiple requests

---
+++ client-async/client/main.go
@@ -0,0 +1,79 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package main
+
+import (
+	"log"
+	// "os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+
+)
+
+var cnt int = 0
+
+func callMultipleAsyncRPC(c pb.GreeterClient) {
+    log.Print("Calling RPC without getting return value but with goroutine")
+    for i := 0; i < 5; i++ {
+        go func() {
+            log.Print("Inside goroutine: Start c.SyaHello", cnt)
+            cnt++
+            c.SayHello(context.Background(), &pb.HelloRequest{Name: "may"})
+            cnt++
+            log.Print("Inside goroutine: Exit c.SayHello", cnt)
+        }()
+    }
+    log.Print("Bless with goroutine")
+}
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	/* name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	} */
+    /* log.Printf("Calling RPC without getting return value")
+	c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+    log.Printf("Not using return values from Remote Procedure call") */
+    callMultipleAsyncRPC(c)
+
+    // log.Printf("Greeting: %s", r.Message)
+
+    /* r, err = c.SayHelloAgain(context.Background(), &pb.HelloRequest{Name: name})
+    if err != nil {
+        log.Fatalf("could not greet: %v", err)
+    }
+    log.Printf("Greeting: %s", r.Message) */
+    for {}
+}
